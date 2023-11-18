ba4b2ac352ac790e3232353438c2697a70dde5fc
Andrey Frolov <senior.slender2015@gmail.com>
2023-11-18 12:54:40 +0200

    Opening and closing animation

--- js/app.js
+++ js/app.js
@@ -21,3 +21,9 @@ const sliderBg = new Swiper('.slider_bg', {
 	slidesPerView: 3.5
 })
 sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider__item').forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('opened')
+	})
+})
