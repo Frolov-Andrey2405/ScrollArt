82a939358bcbbfe2c06ed15385f79f6d35c77e08
Andrey Frolov <senior.slender2015@gmail.com>
2023-11-18 12:11:14 +0200

    Adaptive slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 const sliderMain = new Swiper('.slider_main', {
     freeMode: true,
 	centeredSlides: true,
-	slidesPerView: 3.5,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
 })
